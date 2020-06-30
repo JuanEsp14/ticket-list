74e91be313362abfc4c560a8fe6ed7f9b6f69be9
Juan Espinoza <juan.espinoza@fluxit.com.ar>
2020-06-30 19:46:08 -0300

    Ticket class added and push done when I create a new ticket

--- server/classes/ticket.controller.js
+++ server/classes/ticket.controller.js
@@ -1,24 +1,36 @@
 const fs = require('fs');
 
+class Ticket {
+    constructor(number, desk) {
+        this.number = number;
+        this.desk = desk;
+    }
+}
+
 class TicketController {
     constructor() {
         let data = require('../data/data.json');
         if (data.today != this.today) {
             this.saveData({
                 last: 0,
-                today: new Date().getDate()
+                today: new Date().getDate(),
+                tickets: []
             });
             data = require('../data/data.json');
         }
         this.today = data.today;
         this.last = data.last;
+        this.tickets = data.tickets;
     }
 
     next() {
         this.last++;
+        let ticket = new Ticket(this.last, null);
+        this.tickets.push(ticket);
         this.saveData({
             last: this.last,
-            today: this.today
+            today: this.today,
+            tickets: this.tickets
         });
 
         return `Ticket ${this.last}`;
